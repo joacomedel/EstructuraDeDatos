d6ae41746b8025f71b6f8e591cfce0609502d1e3
joacomedel <joaquinmedel1234@gmail.com>
2022-05-19 23:11:36 -0300

    commit on 'heap'

--- src/conjuntista/Heap.java
+++ src/conjuntista/Heap.java
@@ -3,7 +3,7 @@ package conjuntista;
 public class Heap {
     int tamanio;
     int []heap;
-    int ultimo = -1;
+    int ultimo = 0;
     //minimo
     public Heap(int tamanio){
         this.tamanio = tamanio;
@@ -12,18 +12,34 @@ public class Heap {
     //Arreglo hijo izquierdo , posicion 2n
     //Arreglo hijo derecho , posicion 2n + 1
     //Padre , n/2
-    public void insertar(int num){
-        poner(num);
-        ordenar(num);
-    }
-    private void poner(int num){
-        if (this.esVacio()) {
-            
+    public boolean insertar(int num){
+        boolean puesto;
+        puesto = poner(num);
+        if (puesto) {
+            ordenar();
         }
+        return puesto;
+    }
+    private boolean poner(int num){
+        boolean puesto = false;
+        if (!this.esLleno()) {
+            heap[ultimo+1] = num;
+            ultimo = ultimo+1;
+            puesto = true;
+        }
+        return puesto;
     
     }
-    private void ordenar(int num){
-
+    private void ordenar(){
+        int posicion = ultimo;
+        while (heap[posicion] < heap[(posicion/2)]) {
+            int temp = heap[posicion+1];
+            heap[posicion+1] = heap[posicion];
+            heap[posicion] = temp;
+            posicion= posicion/2;
+        }
+            
+        
     }
 
     public void elimiarCima() {
@@ -34,5 +50,8 @@ public class Heap {
     }
     public boolean esVacio(){
         return ultimo == -1;
+    }
+    public boolean esLleno(){
+        return ultimo == heap.length-1;
     }
 }
