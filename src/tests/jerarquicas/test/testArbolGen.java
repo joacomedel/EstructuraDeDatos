9bc07f316e990f35319abd2b1d770f0c928677ac
joacomedel <joaquinmedel1234@gmail.com>
2022-06-03 11:55:02 -0300

    commit on 'simulacro'

--- src/tests/jerarquicas/test/testArbolGen.java
+++ src/tests/jerarquicas/test/testArbolGen.java
@@ -5,19 +5,20 @@ import lineales.dinamicas.Lista;
 public class testArbolGen {
     public static void main(String[] args) {
         ArbolGen arbol = new ArbolGen();
-        /*arbol.insertar("a","a");
-        arbol.insertar("b","a");
-        arbol.insertar("c","a");
-        arbol.insertar("d","a");
-        arbol.insertar("e","b");
-        arbol.insertar("f","d");
-        arbol.insertar("g","d");
-        arbol.insertar("h","e");
-        arbol.insertar("i","e");
-        arbol.insertar("j","e");
-        arbol.insertar("k","i");
-        System.out.println(arbol);
-        System.out.println(arbol.listarPorNiveles());*/
+        arbol.insertar('A', 'S');
+        arbol.insertar('B', 'A');
+        arbol.insertar('D', 'B');
+        arbol.insertar('C', 'A');
+        arbol.insertar('E', 'C');
+        arbol.insertar('G', 'E');
+        arbol.insertar('H', 'E');
+        arbol.insertar('F', 'C');
+        Lista lista = new Lista();
+        lista.insertar('A', 1);
+        lista.insertar('C', 2);
+        lista.insertar('E', 3);
+        System.out.println(arbol.verificarCamino(lista)); 
+
     }
     
 }
