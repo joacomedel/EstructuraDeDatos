02c7b8326cea335ef0f41af84c21cd3ab1198ea3
joacomedel <joaquinmedel1234@gmail.com>
2022-06-10 11:51:42 -0300

    commit on 'Simulacro viernes'

--- src/conjuntista/ABB.java
+++ src/conjuntista/ABB.java
@@ -2,59 +2,61 @@ package conjuntista;
 
 import java.util.Optional;
 
-import lineales.dinamicas.Nodo;
+import lineales.dinamicas.Lista;
 
 public class ABB {
     NodoArbol raiz;
 
-    public ABB (){
+    public ABB() {
         raiz = null;
     }
-    public boolean insertar(Comparable comp){
+
+    public boolean insertar(Comparable comp) {
         boolean exito = false;
         if (this.esVacio()) {
             raiz = new NodoArbol(comp);
-        }{
-            exito = insertarAux(comp , raiz); 
         }
-        
+        {
+            exito = insertarAux(comp, raiz);
+        }
+
         return exito;
     }
 
-    public boolean insertarAux(Comparable comp , NodoArbol nodo){
+    public boolean insertarAux(Comparable comp, NodoArbol nodo) {
         boolean exito = false;
         int compareTo = nodo.getElem().compareTo(comp);
         if (compareTo != 0) {
             if (compareTo > 0) {
-                //Si es menor voy a la izquierda
+                // Si es menor voy a la izquierda
                 if (nodo.getIzq() == null) {
                     nodo.setIzq(new NodoArbol(comp));
                     exito = true;
-                }else{
+                } else {
                     exito = insertarAux(comp, nodo.getIzq());
-                } 
-            }else{
-                //Si es mayor voy a la derecha
+                }
+            } else {
+                // Si es mayor voy a la derecha
                 if (nodo.getDer() == null) {
                     nodo.setDer(new NodoArbol(comp));
                     exito = true;
-                }else{
+                } else {
                     exito = insertarAux(comp, nodo.getDer());
-                } 
+                }
             }
         }
         return exito;
     }
 
-    private int obtenerCaso(NodoArbol nodo){
+    private int obtenerCaso(NodoArbol nodo) {
         int caso;
         if (nodo.getIzq() == null && nodo.getDer() == null) {
-            caso = 1; //Caso 1 de la teoria , es hoja
-        }else{
-            if (nodo.getIzq() == null && nodo.getDer() !=null || (nodo.getIzq() != null && nodo.getDer() == null)) {
-                caso = 2; //Caso 2 
-            }else{
-                caso = 3; //Caso 3 tiene dos hijos
+            caso = 1; // Caso 1 de la teoria , es hoja
+        } else {
+            if (nodo.getIzq() == null && nodo.getDer() != null || (nodo.getIzq() != null && nodo.getDer() == null)) {
+                caso = 2; // Caso 2
+            } else {
+                caso = 3; // Caso 3 tiene dos hijos
             }
         }
         return caso;
@@ -63,37 +65,38 @@ public class ABB {
     public boolean pertenece(Comparable comp) {
         boolean pertenece = false;
         if (!this.esVacio()) {
-            if (this.raiz.getElem().compareTo(comp) == 0){
+            if (this.raiz.getElem().compareTo(comp) == 0) {
                 pertenece = true;
-            }else{
+            } else {
                 pertenece = perteneceAux(comp, this.raiz);
             }
         }
         return pertenece;
     }
 
-    private boolean perteneceAux(Comparable comp , NodoArbol nodo){
-        boolean pertenece = false;;
+    private boolean perteneceAux(Comparable comp, NodoArbol nodo) {
+        boolean pertenece = false;
+        ;
         int compareTo = nodo.getElem().compareTo(comp);
         if (compareTo == 0) {
             pertenece = true;
-        }else{
+        } else {
             if (compareTo > 0) {
-                //Si es menor voy a la izquierda
+                // Si es menor voy a la izquierda
                 if (nodo.getIzq() != null) {
-                    pertenece = perteneceAux(comp,nodo.getIzq());
+                    pertenece = perteneceAux(comp, nodo.getIzq());
                 }
-            }else{
-                //Si es mayor voy a la derecha
+            } else {
+                // Si es mayor voy a la derecha
                 if (nodo.getDer() != null) {
-                    pertenece = perteneceAux(comp,nodo.getDer());
+                    pertenece = perteneceAux(comp, nodo.getDer());
                 }
             }
         }
         return pertenece;
     }
 
-    public boolean esVacio(){
+    public boolean esVacio() {
         return raiz == null;
     }
 
@@ -106,52 +109,55 @@ public class ABB {
     }
 
     public boolean eliminar(Comparable comp) {
-        boolean exito= false;
+        boolean exito = false;
         if (!this.esVacio()) {
             exito = eliminarAux(comp, this.raiz);
         }
         return exito;
     }
-    private boolean eliminarAux(Comparable comp , NodoArbol nodo) {
+
+    private boolean eliminarAux(Comparable comp, NodoArbol nodo) {
         boolean exito = false;
         int compareTo = nodo.getElem().compareTo(comp);
         if (compareTo == 0) {
             int caso = this.obtenerCaso(nodo);
             switch (caso) {
                 case 1:
-                intercambiar(nodo, null,'');
-                break;
+                    intercambiar(nodo, null, ' ');
+                    break;
                 case 2:
-                if (nodo.getIzq() != null) {
-                    intercambiar(nodo, nodo.getIzq(),'I');
-                }else{
-                    intercambiar(nodo,nodo.getDer(),'D');
-                }
-                
-                break;
+                    if (nodo.getIzq() != null) {
+                        intercambiar(nodo, nodo.getIzq(), 'I');
+                    } else {
+                        intercambiar(nodo, nodo.getDer(), 'D');
+                    }
+
+                    break;
                 case 3:
-                nodo = remplazarPorCandidato(nodo.getIzq());
-                break;
+                    nodo = remplazarPorCandidato(nodo.getIzq());
+                    break;
             }
             exito = true;
-        }else{
+        } else {
             if (compareTo > 0) {
                 exito = eliminarAux(comp, nodo.getIzq());
-            }else{
+            } else {
                 exito = eliminarAux(comp, nodo.getDer());
             }
         }
         return exito;
     }
-    private void intercambiar(NodoArbol nodo,NodoArbol nodo2, char c){
-       
+
+    private void intercambiar(NodoArbol nodo, NodoArbol nodo2, char c) {
+
     }
-    private NodoArbol remplazarPorCandidato(NodoArbol nodo){
+
+    private NodoArbol remplazarPorCandidato(NodoArbol nodo) {
         NodoArbol retorna = null;
         if (nodo.getDer() == null) {
             retorna = nodo;
             nodo = null;
-        }else{
+        } else {
             remplazarPorCandidato(nodo.getDer());
         }
         return retorna;
@@ -175,5 +181,37 @@ public class ABB {
             cadena = stringAux(nodo.getDer(), cadena);
         return cadena;
     }
-}
 
+    public Lista listarMayorIgual(Comparable comp) {
+        Lista lista = new Lista();
+        if (!this.esVacio()) {
+            listarMayorIgualAux(lista, this.raiz, comp);
+        }
+        return lista;
+    }
+
+    private void listarMayorIgualAux(Lista lista, NodoArbol nodo, Comparable comp) {
+        if (nodo != null) {
+            int compareTo = nodo.getElem().compareTo(comp);
+            if (compareTo >= 0) {
+                listarMayorIgualAux2(lista, nodo, comp);
+            } else {
+                listarMayorIgualAux(lista, nodo.getDer(), comp);
+            }
+        }
+    }
+
+    private void listarMayorIgualAux2(Lista lista, NodoArbol nodo,Comparable comp) {
+        if (nodo != null) {
+            Boolean esMayorIgual = nodo.getElem().compareTo(comp) >= 0;
+            if (esMayorIgual) {
+                listarMayorIgualAux2(lista, nodo.getIzq(), comp);
+                lista.insertar(nodo.getElem(), 1);
+                listarMayorIgualAux2(lista, nodo.getDer(), comp);
+            }else{
+                listarMayorIgualAux2(lista, nodo.getDer(), comp);
+            }
+        }
+    }
+
+}
--- src/tests/conjuntista/testABB.java
+++ src/tests/conjuntista/testABB.java
@@ -3,15 +3,19 @@ import conjuntista.ABB;
 public class testABB {
     public static void main(String[] args) {
         ABB arbol = new ABB();
-        arbol.insertar(0);
-        arbol.insertar(3);
-        arbol.insertar(4);
-        arbol.insertar(1);
-        arbol.insertar(6);
+        arbol.insertar(10);
         arbol.insertar(5);
-        arbol.insertar(2);
+        arbol.insertar(1);
+        arbol.insertar(14);
+        arbol.insertar(12);
+        arbol.insertar(17);
+        arbol.insertar(13);
+        arbol.insertar(15);
+        arbol.insertar(20);
+        arbol.insertar(11);
         System.out.println(arbol);
-        System.out.println(arbol.eliminar(0));
+        System.out.println(arbol.listarMayorIgual(13));
+
         
     }
 }
